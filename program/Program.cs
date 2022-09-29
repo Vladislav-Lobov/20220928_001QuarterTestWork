7c3ea006eedee6a8a5620472d9b6d1e17396a887
Vladislav <nkamsk777@yandex.ru>
2022-09-29 16:15:38 +0300

    Commit007

---
+++ program/Program.cs
@@ -0,0 +1,26 @@
+﻿string[] FillArray(int size)
+{
+    string input = string.Empty;
+    string[] array = new string[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"Введите элемент массива с индексом {i}");
+        do
+        {
+            input = Console.ReadLine();
+        } while (isNotCorrect(input));
+
+        array[i] = input;
+    }
+    return array;
+}
+
+bool isNotCorrect(string symbol)
+{
+    if (symbol == string.Empty)
+    {
+        Console.WriteLine("Некорректный ввод, повторите:");
+        return true;
+    }
+    return false;
+}
