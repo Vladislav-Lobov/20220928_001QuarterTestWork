9679acb4501fb30a37352dabb64847cf358c35f0
Vladislav <nkamsk777@yandex.ru>
2022-09-29 16:17:19 +0300

    Commit008

--- program/Program.cs
+++ program/Program.cs
@@ -24,3 +24,40 @@ bool isNotCorrect(string symbol)
     }
     return false;
 }
+void PrintArray(string[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.Write("]");
+}
+
+int LengthCount(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+string[] GetNewArray(int size, string[] inputArray)
+{
+    string[] outputArray = new string[size];
+    int j = 0;
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        if (inputArray[i].Length <= 3)
+        {
+            outputArray[j] = inputArray[i];
+            j++;
+        }
+    }
+    return outputArray;
+}
